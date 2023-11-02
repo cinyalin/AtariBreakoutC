a4c77f2704872924f2a02a83a35bd6941857a609
cinyalin <cinyalin8@gmail.com>
2023-11-02 12:04:33 -0400

    initial commit

---
+++ README.txt
@@ -0,0 +1,13 @@
+# HOW TO PLAY
+BRICKS		break all the bricks to win
+LIVES		lose lives by hitting the bottom of the screen
+		lose 3 lives and GAME OVER
+
+
+# CONTROLS
+ENTER		starts the game (START)
+BACKSPACE	restart the game at any time (SELECT)
+LEFT		moves the player left
+RIGHT		moves the player right
+UP		moves the player up
+DOWN		moves the player down
---
+++ images/playa.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=60x5 playa playa.png 
+ * Time-stamp: Monday 07/17/2023, 21:35:59
+ * 
+ * Image Information
+ * -----------------
+ * playa.png 60@5
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef PLAYA_H
+#define PLAYA_H
+
+extern const unsigned short playa[300];
+#define PLAYA_SIZE 600
+#define PLAYA_LENGTH 300
+#define PLAYA_WIDTH 60
+#define PLAYA_HEIGHT 5
+
+#endif
+
